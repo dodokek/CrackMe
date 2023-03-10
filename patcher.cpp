3cb14c4f565b4c672f520fe7643447b5608ef12f
Aleksander Morozov <alex.morozov.dd@mail.ru>
2023-03-10 12:06:28 +0300

    wrote patcher to vlados file

--- patcher.cpp
+++ patcher.cpp
@@ -1,35 +1,32 @@
-#include <SFML/Window.hpp>
-
 #include "include/patcher.h"
 
 
 int main()
 {
-    sf::Window window(sf::VideoMode(800, 600), "My window");
 
 
-    // char* buffer_res = (char*) calloc (MAX_BIN_FILE_LEN, sizeof(char));
-    // patch_comfile(buffer_res);
+    char* buffer_res = (char*) calloc (MAX_BIN_FILE_LEN, sizeof(char));
+    patch_comfile(buffer_res);
 
-    // FILE* com_file = get_file ("vlads.com", "r");
-    // char* buffer_origin = (char*) calloc (MAX_BIN_FILE_LEN, sizeof(char));
-    // int file_len = read_file (com_file, &buffer_origin);
-    // close_file (com_file, "vlads.com");
+    FILE* com_file = get_file ("vlads.com", "rb");
+    char* buffer_origin = (char*) calloc (MAX_BIN_FILE_LEN, sizeof(char));
+    int file_len = read_file (com_file, &buffer_origin);
+    close_file (com_file, "vlads.com");
 
-    // for (int i = 0; i < file_len; i++)
-    // {
-    //     if (buffer_origin[i] != buffer_res[i])
-    //     {
-    //         printf ("Difference in i = %d, origin: %c, patched: %c\n",
-    //                 i, buffer_origin[i], buffer_res[i]);
-    //     }
-    // }
+    for (int i = 0; i < file_len; i++)
+    {
+        if (buffer_origin[i] != buffer_res[i])
+        {
+            printf ("Difference in i = %d, origin: %c, patched: %c\n",
+                    i, buffer_origin[i], buffer_res[i]);
+        }
+    }
 
-    // free (buffer_origin);
-    // buffer_origin = nullptr;
+    free (buffer_origin);
+    buffer_origin = nullptr;
 
-    // free (buffer_res);
-    // buffer_res = nullptr;
+    free (buffer_res);
+    buffer_res = nullptr;
 
     return 0;
 }
@@ -37,7 +34,7 @@ int main()
 
 void patch_comfile (char* buffer_res)
 {
-    FILE* com_file = get_file ("vlads.com", "r");
+    FILE* com_file = get_file ("vlads.com", "rb");
 
     int file_len = read_file (com_file, &buffer_res);
     close_file (com_file, "vlads.com");
@@ -52,14 +49,14 @@ void patch_comfile (char* buffer_res)
             {
                 printf ("Patching...\n");
 
-                // buffer_res[i] = 0x3B;
-                // buffer_res[i+1] = 0xC0;
-                // buffer_res[i+2] = 0x90;
+                buffer_res[i] = 0x3B;
+                buffer_res[i+1] = 0xC0;
+                buffer_res[i+2] = 0x90;
             }
         }
     }
 
-    com_file = get_file ("vlads_cr.com", "w+");
+    com_file = get_file ("vlads_cr.com", "wb+");
     fwrite(buffer_res, sizeof (char), file_len, com_file);
     close_file (com_file, "vlads_cr.com");
 }
